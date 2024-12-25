9d16cdc7070dbe9b9e3ecbd28786319bb12fb613
Yusuf Gül <xusufgul@gmail.com>
2024-12-25 04:41:10 +0000

    garnet: YAAP bringup

--- yaap_garnet.mk
+++ yaap_garnet.mk
@@ -9,7 +9,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common DerpFest stuff.
-$(call inherit-product, vendor/derp/config/common_full_phone.mk)
+$(call inherit-product, vendor/yaap/config/common_full_phone.mk)
 
 # Inherit from garnet device
 $(call inherit-product, device/xiaomi/garnet/device.mk)
@@ -25,7 +25,7 @@ DERP_BUILDTYPE := Official
 #GApps
 TARGET_USES_PICO_GAPPS := true
 
-PRODUCT_NAME := derp_garnet
+PRODUCT_NAME := yaap_garnet
 PRODUCT_DEVICE := garnet
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Redmi
