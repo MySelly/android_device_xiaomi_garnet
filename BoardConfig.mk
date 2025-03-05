7abc012586dceecaa68e9e31d4da6fd02f5deaf1
New Author Name <120115258+swiitchOFF@users.noreply.github.com>
2025-03-06 01:21:20 +0530

    garnet: Include Sony Dolby Atmos
    * Using dolby from: https://github.com/swiitch-OFF-Lab/hardware_dolby
    [1]: sm8350-common: Add dolby volume listener effect
    [2]: sm8350-common: audio: Bring all stream effects
    
    Co-Authored-By: HELLBOY017 <abhaygill017@gmail.com>
    Signed-off-by: swiitchOFF <120115258+swiitchOFF@users.noreply.github.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -76,13 +76,13 @@ TARGET_GLOBAL_OPTIMIZATION := O3
 TARGET_GLOBAL_THINLTO := true
 
 # HIDL
-DEVICE_MATRIX_FILE := $(DEVICE_PATH)/configs/hidl/compatibility_matrix.xml
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
+DEVICE_MATRIX_FILE += $(DEVICE_PATH)/configs/hidl/compatibility_matrix.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
     hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml \
     vendor/lineage/config/device_framework_matrix.xml
 
-DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/configs/hidl/manifest.xml
+DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/configs/hidl/manifest.xml
 
 $(foreach sku, CN GL JP, \
     $(eval ODM_MANIFEST_SKUS += $(sku)) \
