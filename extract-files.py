9052f2d7086fd2bfe438db7a9d5e09a1374bf81f
unmoved21 <unmoved2121@gmail.com>
2025-06-22 23:27:57 +0200

    Revert "garnet: Move most agm and pal targets to source built"
    This reverts commit 01709253538c20f18870b932fd785aeeda636b59.
    
    Change-Id: I3313b5708bb3b55ef3495fb8d6bfea4ce5235b95

--- extract-files.py
+++ extract-files.py
@@ -47,7 +47,6 @@ lib_fixups: lib_fixups_user_type = {
     ): lib_fixup_vendor_suffix,
     (
         'libagmclient',
-        'libagmmixer',
         'libwifi-hal-ctrl',
         'libpalclient',
         'vendor.qti.hardware.pal@1.0-impl',
